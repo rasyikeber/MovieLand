3f703f4bdc86e3adffbadde9393ab1da37de6f60
RasYikber <125854836+rasyikeber@users.noreply.github.com>
2023-10-01 21:52:25 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,72 +1,72 @@
-import React, { useState, useEffect } from "react";
-import MovieCard from './MovieCard';
-import './App.css'
-import searchIcon from './search.svg'
+// import React, { useState, useEffect } from "react";
+// import MovieCard from './MovieCard';
+// import './App.css'
+// import searchIcon from './search.svg'
 
-const API_URL = 'http://www.omdbapi.com?apikey=43d74137';
+// const API_URL = 'http://www.omdbapi.com?apikey=43d74137';
 
-const App = () => {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [movies, setMovies] = useState([]);
-  const [filteredMovies, setFilteredMovies] = useState([]);
+// const App = () => {
+//   const [searchTerm, setSearchTerm] = useState("");
+//   const [movies, setMovies] = useState([]);
+//   const [filteredMovies, setFilteredMovies] = useState([]);
 
-  const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s=${title}`);
-    const data = await response.json();
+//   const searchMovies = async (title) => {
+//     const response = await fetch(`${API_URL}&s=${title}`);
+//     const data = await response.json();
 
-    setMovies(data.Search);
-  }
+//     setMovies(data.Search);
+//   }
 
-  useEffect(() => {
-    searchMovies('spiderman');
-  }, []);
+//   useEffect(() => {
+//     searchMovies('spiderman');
+//   }, []);
 
-  useEffect(() => {
-    if (searchTerm.trim() === "") {
-      setFilteredMovies([]);
-    } else {
-      const filtered = movies.filter(movie =>
-        movie.Title.toLowerCase().includes(searchTerm.toLowerCase())
-      );
-      setFilteredMovies(filtered);
-    }
-  }, [searchTerm, movies]);
+//   useEffect(() => {
+//     if (searchTerm.trim() === "") {
+//       setFilteredMovies([]);
+//     } else {
+//       const filtered = movies.filter(movie =>
+//         movie.Title.toLowerCase().includes(searchTerm.toLowerCase())
+//       );
+//       setFilteredMovies(filtered);
+//     }
+//   }, [searchTerm, movies]);
 
-  return (
-    <div className='app'>
-      <h1>Movie Land</h1>
-      <div className='search'>
-        <input
-          placeholder='search for movie'
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <img
-          src={searchIcon}
-          alt='search'
-          onClick={() => searchMovies(searchTerm)}
-        />
-      </div>
-      {(filteredMovies.length > 0 || searchTerm.trim() === "") ? (
-        <div className='container'>
-          {filteredMovies.length > 0
-            ? filteredMovies.map((movie) => (
-                <MovieCard key={movie.imdbID} movie={movie} />
-              ))
-            : movies.map((movie) => (
-                <MovieCard key={movie.imdbID} movie={movie} />
-              ))}
-        </div>
-      ) : (
-        <div className="empty">
-          <h2>no movie found</h2>
-        </div>
-      )}
-    </div>
-  );
-}
+//   return (
+//     <div className='app'>
+//       <h1>Movie Land</h1>
+//       <div className='search'>
+//         <input
+//           placeholder='search for movie'
+//           value={searchTerm}
+//           onChange={(e) => setSearchTerm(e.target.value)}
+//         />
+//         <img
+//           src={searchIcon}
+//           alt='search'
+//           onClick={() => searchMovies(searchTerm)}
+//         />
+//       </div>
+//       {(filteredMovies.length > 0 || searchTerm.trim() === "") ? (
+//         <div className='container'>
+//           {filteredMovies.length > 0
+//             ? filteredMovies.map((movie) => (
+//                 <MovieCard key={movie.imdbID} movie={movie} />
+//               ))
+//             : movies.map((movie) => (
+//                 <MovieCard key={movie.imdbID} movie={movie} />
+//               ))}
+//         </div>
+//       ) : (
+//         <div className="empty">
+//           <h2>no movie found</h2>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
 
 
 
@@ -154,71 +154,71 @@ export default App;
 
 
 
-// import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
-// import MovieCard from './MovieCard';
+import MovieCard from './MovieCard';
 
-// import './App.css'
-// import searchIcon from './search.svg'
+import './App.css'
+import searchIcon from './search.svg'
 
-// const API_URL ='http://www.omdbapi.com?apikey=43d74137';
-
-
+const API_URL ='http://www.omdbapi.com?apikey=43d74137';
 
 
 
 
 
 
-// const App = () => {
-
-//     const [searchTerm, setSearchTerm] = useState("");
-//     const [movies, setMovies] = useState([]);
-
-//    const searchMovies = async(title)=> {
-//     const response =await fetch(`${API_URL}&s=${title}`);
-//     const data= await response.json();
-
-//     setMovies(data.Search);
-//    }
-
-//     useEffect( ()=> {
-//     searchMovies('spiderman');
-//     }, []);
 
 
-//     return(
-//       <div className='app'>
-//         <h1>Movie Land</h1>
-//         <div className='search'>
-//             <input
-//             placeholder='search for movie'
-//             value={searchTerm}
-//             onChange={(e) => setSearchTerm(e.target.value)}
-//             />
-//             <img
-//             src={searchIcon}
-//             alt='search'
-//             onClick={() => searchMovies(searchTerm)}
-//             />
-//             </div>
-//             {
-//                 movies?.length >0 ? (
+const App = () => {
+
+    const [searchTerm, setSearchTerm] = useState("");
+    const [movies, setMovies] = useState([]);
+
+   const searchMovies = async(title)=> {
+    const response =await fetch(`${API_URL}&s=${title}`);
+    const data= await response.json();
+
+    setMovies(data.Search);
+   }
+
+    useEffect( ()=> {
+    searchMovies('spiderman');
+    }, []);
 
 
-//             <div className='container'>
-//               { movies.map((movie)=> (
-//                 <MovieCard movie={movie} />
-//               ))}
-//             </div>
-//                 ): (
-//                     <div className="empty">
-//                         <h2>no movie found</h2>
-//                     </div>
-//                 )
-//             }
+    return(
+      <div className='app'>
+        <h1>Movie Land</h1>
+        <div className='search'>
+            <input
+            placeholder='search for movie'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <img
+            src={searchIcon}
+            alt='search'
+            onClick={() => searchMovies(searchTerm)}
+            />
+            </div>
+            {
+                movies?.length >0 ? (
+
+
+            <div className='container'>
+              { movies.map((movie)=> (
+                <MovieCard movie={movie} />
+              ))}
+            </div>
+                ): (
+                    <div className="empty">
+                        <h2>no movie found</h2>
+                    </div>
+                )
+            }
         
-//       </div>
-//     );
-// }
-// export default App;
+      </div>
+    );
+}
+export default App;
